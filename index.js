9209fb5cf1ac2b37fc1ddba74fd164565bc251fb
Jiri De Jagere <jiri.dejagere@softwareag.com>
2023-03-20 13:20:54 +0100

    core CLI implementation

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /user/bin/env node
+
+const figlet = require('figlet');
+const program = require('commander');
+
+const commands = require('./lib/index');
+
+function setupCli() {
+  console.log(figlet.textSync('DELIVER', {
+    font: 'Doom'
+  }));
+  
+  commands.forEach(c => {
+    const cdef = c.definition();
+
+    const command = program
+      .command(cdef.command)
+      .description(cdef.description);
+
+    cdef.arguments.forEach(arg => {
+      command.argument(arg[0], arg[1]);
+    });
+
+    cdef.options.forEach(opt => {
+      command.option([opt[0], opt[1]].join(','), opt[2], opt[3]);
+    });
+
+    command.action(function () {
+      c.action.apply(c, arguments);
+      console.log(c.getResult());
+    });
+  });
+}
+
+setupCli();
+program.parse();
